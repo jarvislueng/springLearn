2171a9d3dab436199628e6b27cf9784ebfe504be
jarvis <jarvisLueng@gmail.com>
2019-04-28 13:51:13 +0800

    测试FeignConfiguration

--- hystrix/feign-hystrix/src/main/java/com/jarvis/springclound/hystrix/feignhystrix/controller/HiController.java
+++ hystrix/feign-hystrix/src/main/java/com/jarvis/springclound/hystrix/feignhystrix/controller/HiController.java
@@ -1,6 +1,7 @@
 package com.jarvis.springclound.hystrix.feignhystrix.controller;
 
 import com.jarvis.springclound.hystrix.feignhystrix.service.SchedualServiceHi;
+import com.jarvis.springclound.hystrix.feignhystrix.service.SchedualServiceHi2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +12,17 @@ public class HiController {
     @Autowired
     SchedualServiceHi schedualServiceHi;
 
+    @Autowired
+    SchedualServiceHi2 schedualServiceHi2;
+
     @RequestMapping("/hi")
     public String hi(@RequestParam("name")String name){
         return schedualServiceHi.sayHiFromClientOne(name);
+    }
+
+
+    @RequestMapping("/hi2")
+    public String hi2(@RequestParam("name")String name){
+        return schedualServiceHi2.sayHiFromClientOne(name);
     }
 }
